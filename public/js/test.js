7b428147127b5de336ebc6f765fb80161619877b
aa14099 <yatsuka14863@gmail.com>
2021-02-11 16:01:48 +0900

    gitignore

--- public/js/test.js
+++
@@ -1,9 +0,0 @@
-function OnButtonClick() {
-     target = document.getElementById("output");
-     buf = document.getElementById("buf")
-     if(target.innerHTML == "Penguin"){
-       target.innerHTML=buf.value;
-     }else{
-       target.innerHTML="Penguin";
-     }
-   }
