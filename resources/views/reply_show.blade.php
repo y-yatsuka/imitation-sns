62c7a57e0225d62ce0f2018016c61a04cf54881e
aa14099 <yatsuka14863@gmail.com>
2021-02-19 17:35:09 +0900

    レイアウト、ページ遷移を変更

--- resources/views/reply_show.blade.php
+++ resources/views/reply_show.blade.php
@@ -9,57 +9,71 @@
 
 @section('content')
 <script src={{ asset('js/replyGoodFunction.js')}}></script>
-@if($parent->user->image!=null)
-<img src="{{ asset('storage/profile_images/'.$parent->user->id.'.jpeg') }}" class="round-image small" />
-@else
-<img src="{{ asset('images/profile_icon.png') }}" class="round-image small" />
-@endif
-<h3>{{ $parent->user->name }}</h3>
-<p>
-  {!! nl2br(e($parent->content)) !!}
-</p>
-@if($flag)
-  @if($parent->image!==null)
-  <a href="{{ route('article.image',['id' => $parent->id]) }} ">
-    <div class="image">
-        <img src="{{asset('storage/article_images/'.$parent->image)}}" />
-    </div>
-  </a>
-  @endif
-@endif
-<br />
-
-<div>
-  <p>
-    {{ $parent->user->name }}さんへの返信
-  </p>
+<div class="article-show">
+  <div class="inner">
+    <a href="{{ route('user.detail',['user_id' => $parent->user->id]) }}">
+      @if($parent->user->image!=null)
+      <img src="{{ asset('storage/profile_images/'.$parent->user->id.'.jpeg') }}" class="round-image small" />
+      @else
+      <img src="{{ asset('images/profile_icon.png') }}" class="round-image small" />
+      @endif
+    </a>
+    <a href="{{ route('user.detail',['user_id' => $parent->user->id]) }}">
+      <h3>{{ $parent->user->name }}</h3>
+    </a>
+    @if($flag)
+    <a href="{{ route('article.detail', ['article_id' => $parent->id ]) }}">
+      <p>
+        {!! nl2br(e($parent->content)) !!}
+      </p>
+    </a>
+    @else
+    <a href="{{ route('reply.detail', ['reply_id' => $parent->id ]) }}">
+      <p>
+        {!! nl2br(e($parent->content)) !!}
+      </p>
+    </a>
+    @endif
+  </div>
+    @if($parent->image!==null)
+    <a href="{{ route('article.image',['id' => $parent->id]) }} ">
+      <div class="small-image">
+          <img src="{{asset('storage/article_images/'.$parent->image)}}" />
+      </div>
+    </a>
+    @endif
 </div>
-@if($reply->user->image!=null)
-<img src="{{ asset('storage/profile_images/'.$reply->user->id.'.jpeg') }}" class="round-image middle" />
-@else
-<img src="{{ asset('images/profile_icon.png') }}" class="round-image middle" />
-@endif
-<h1>{{ $reply->user->name }}</h1>
-<h3>
-  {!! nl2br(e($reply->content)) !!}
-</h3>
-<a href=# id='goodCount' onload='goodCount()'>いいね!：{{ $goodCount }}件</a>
-<br />
-@auth
-<button class='btn btn-outline-primary' id='goodButton'>
-  @if($favorite)
-  いいねを取り消す
-  @else
-  いいね!
-  @endif
-</button>
-<a class='btn btn-outline-primary' href={{ route('reply.new', ['id' => $reply->id ]) }} >返信する</a>
-@endauth
-<input type='hidden' value={{ $reply->id }} id='reply_id'>
-<br />
 
-<div>
+<div class="article-show">
+  <h3>
+    {{ $parent->user->name }}さんへの返信
+  </h3>
+  <div class="inner">
+    <a href="{{ route('user.detail',['user_id' => $reply->user->id]) }}">
+      @if($reply->user->image!=null)
+      <img src="{{ asset('storage/profile_images/'.$reply->user->id.'.jpeg') }}" class="round-image middle" />
+      @else
+      <img src="{{ asset('images/profile_icon.png') }}" class="round-image middle" />
+      @endif
+    </a>
+    <a href="{{ route('user.detail',['user_id' => $reply->user->id]) }}">
+      <h1>{{ $reply->user->name }}</h1>
+    </a>
+    <h3>
+      {!! nl2br(e($reply->content)) !!}
+    </h3>
+  </div>
+  <a href=# id='goodCount' onload='goodCount()'>いいね!：{{ $goodCount }}件</a>
+  <br />
   @auth
+  <button class='btn btn-outline-primary' id='goodButton'>
+    @if($favorite)
+    いいねを取り消す
+    @else
+    いいね!
+    @endif
+  </button>
+  <a class='btn btn-outline-primary' href={{ route('reply.new', ['id' => $reply->id ]) }} >返信する</a>
   @if($reply->user->id === $login_id)
   {{ Form::open(['method' => 'delete', 'route' =>['reply.destroy', $reply->id]]) }}
     {{ Form::submit('削除', ['class' => 'btn btn-outline-danger']) }}
@@ -67,7 +81,13 @@
   @endif
   <a class='btn btn-outline-primary' href="javascript:history.back()" id='fixed'>戻る</a>
   @endauth
+</div>
 
+<input type='hidden' value={{ $reply->id }} id='reply_id'>
+<br />
+
+<div>
+  <h3>{{$reply->user->name}}さんへの返信一覧</h3>
   @if(count($children))
   <table class='table table-striped table-hover'>
   @foreach($children as $child)
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,47 +5,55 @@
     right:50;
     bottom:60;
   }
+
 </style>
 @section('content')
 <script src="{{ asset('js/goodFunction.js')}}"></script>
-@if($article->user->image!=null)
-<img src="{{ asset('storage/profile_images/'.$article->user->id.'.jpeg') }}" class="round-image middle" />
-@else
-<img src="{{ asset('images/profile_icon.png') }}" class="round-image middle" />
-@endif
-<h1>{{ $article->user->name }}</h1>
-<h3>
-  {!! nl2br(e($article->content)) !!}
-</h3>
-@if($article->image!=null)
-<a href="{{ route('article.image',['id' => $article->id]) }} ">
-  <div class="image">
-      <img src="{{asset('storage/article_images/'.$article->image)}}" />
+<div class="article-show">
+  <div class="inner">
+    <a href="{{ route('user.detail',['user_id' => $article->user->id]) }}">
+      @if($article->user->image!=null)
+      <img src="{{ asset('storage/profile_images/'.$article->user->id.'.jpeg') }}" class="round-image middle" />
+      @else
+      <img src="{{ asset('images/profile_icon.png') }}" class="round-image middle" />
+      @endif
+    </a>
+    <a href="{{ route('user.detail',['user_id' => $article->user->id]) }}">
+      <h1>{{ $article->user->name }}</h1>
+    </a>
+    <h3>
+      {!! nl2br(e($article->content)) !!}
+    </h3>
+    @if($article->image!=null)
+    <a href="{{ route('article.image',['id' => $article->id]) }} ">
+      <div class="image">
+          <img src="{{asset('storage/article_images/'.$article->image)}}" />
+      </div>
+    </a>
+    @endif
   </div>
-</a>
-@endif
-<a href=# id='goodCount' onload='goodCount()'>いいね!：{{ $goodCount }}件</a>
-<br />
-@auth
-<button class='btn btn-outline-primary' id='goodButton'>
-  @if($favorite)
-  いいねを取り消す
-  @else
-  いいね!
-  @endif
-</button>
-<a class='btn btn-outline-primary' href={{ route('article.reply.new', ['id' => $article->id ]) }}>返信する</a>
-@endauth
-<input type='hidden' value={{ $article->id }} id='article_id'>
-<br />
-
-<div>
+  <a href=# id='goodCount' onload='goodCount()'>いいね!：{{ $goodCount }}件</a>
+  <br />
+  @auth
+  <button class='btn btn-outline-primary' id='goodButton'>
+    @if($favorite)
+    いいねを取り消す
+    @else
+    いいね!
+    @endif
+  </button>
+  <a class='btn btn-outline-primary' href={{ route('article.reply.new', ['id' => $article->id ]) }}>返信する</a>
   @if($article->user->id === $login_id)
   {{ Form::open(['method' => 'delete', 'route' =>['article.destroy', $article->id]]) }}
     {{ Form::submit('削除', ['class' => 'btn btn-outline-danger']) }}
   {{ Form::close() }}
   @endif
-
+  @endauth
+</div>
+<input type='hidden' value={{ $article->id }} id='article_id'>
+<br />
+<div>
+  <h3>{{$article->user->name}}さんへの返信一覧</h3>
   @if($replies)
   @foreach ($replies as $reply)
     <div class="parent">
